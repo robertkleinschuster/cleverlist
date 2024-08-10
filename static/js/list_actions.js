eec3d0a69952076b936e741bf7d3ce4b797ebb78
robertkleinschuster <github@robs.social>
2024-08-10 22:17:59 +0200

    list actions fix for lists with 1 result

--- static/js/list_actions.js
+++ static/js/list_actions.js
@@ -12,11 +12,18 @@ class ListActionsDropdown extends HTMLElement {
         const item = e.target.closest('.list-action-item')
         if (form && item) {
             form[this.dataset.dropdownName][0].value = item.dataset.action;
-            for (const action of form[this.dataset.checkboxName]) {
-                if (action.value === this.dataset.checkboxValue) {
-                    action.checked = true;
+            if (form[this.dataset.checkboxName] instanceof HTMLElement) {
+                if (form[this.dataset.checkboxName].value === this.dataset.checkboxValue) {
+                    form[this.dataset.checkboxName].checked = true;
+                }
+            } else {
+                for (const action of form[this.dataset.checkboxName]) {
+                    if (action.value === this.dataset.checkboxValue) {
+                        action.checked = true;
+                    }
                 }
             }
+
             form.submit()
         }
     }
