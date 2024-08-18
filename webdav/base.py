21e6a938d1f9fc786d3eafbee180175beed22791
robertkleinschuster <github@robs.social>
2024-08-18 21:58:43 +0200

    add global calendars for all users

--- webdav/base.py
+++ webdav/base.py
@@ -416,7 +416,7 @@ class WebDAV(View):
                     request,
                     sub(
                         r"%s$" % (user),
-                        "%s" % (resource.username),
+                        "%s" % (resource.user),
                         request.path.rstrip("/")
                     ) + "/" + resource.name,
                     resource,
