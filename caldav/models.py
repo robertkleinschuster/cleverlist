48a3d08d4cd7cc2fc416a763bc1cf72f684e281c
robertkleinschuster <github@robs.social>
2024-08-24 17:54:22 +0200

    remove sync_token

--- caldav/models.py
+++ caldav/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 import uuid
 
-from django.db.models import F
-
 
 # Create your models here.
 class CalDAVTasklist(models.Model):
@@ -10,10 +8,4 @@ class CalDAVTasklist(models.Model):
     code = models.CharField(max_length=255, unique=True, null=True, blank=True)
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     etag = models.CharField(max_length=255)
-    sync_token = models.IntegerField(default=1)
     last_modified = models.DateTimeField(auto_now=True)
-
-    def save(self, *args, **kwargs):
-        if self.pk:
-            self.sync_token = F('sync_token') + 1
-        super().save(*args, **kwargs)
